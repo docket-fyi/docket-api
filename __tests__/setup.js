6a6293168742d889e2db0b768f2adacc9139e237
John Goldsmith <johngoldsm1@gmail.com>
2020-01-08 16:35:42 -0800

    ci(travis): debug failures

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,5 +1,2 @@
 // This file is referenced in jest.config.js
 require('dotenv').config({path: `${process.cwd()}/.env.test`})
-console.log('start '.repeat(10))
-console.log(process.env)
-console.log('end   '.repeat(10))
