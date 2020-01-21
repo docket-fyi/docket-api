900a33a824cca38ab13a3ed23bee2283e8cd50c3
John Goldsmith <johngoldsm1@gmail.com>
2020-01-21 14:39:57 -0800

    feat(secrets): implement Vault and other secret management tooling

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('./src/v1/config/sentry')
 
+const nodeVault = require('node-vault')
+
 const environment = require('./src/v1/environment')
 const debug = require('./src/v1/config/debug')
 // const subscribeToRedisExpiredKeyEvents = require('./src/v1/config/redis')
@@ -8,11 +10,13 @@ const io = require('./src/v1/config/socket-io')
 const eventHandlers = require('./src/v1/redis')
 const { subscriber } = require('./src/v1/config/redis')
 const socketIOEventHandlerMapping = require('./src/v1/socket-io')
+const vault = require('./src/v1/config/vault')
 
 const redisEventHandlerMapping = new Map([
   [`__keyevent@${environment.redis.databaseId}__:expired`, eventHandlers.keyEventExpired],
   [`unknown`, eventHandlers.unknown]
 ])
+
 subscriber.subscribe(`__keyevent@${environment.redis.databaseId}__:expired`)
 subscriber.on('message', (event, key) => {
   debug.redis(`received '${event}' event for key '${key}'`)
@@ -33,11 +37,62 @@ io.on('connection', socket => {
       handler(socket, data)
     })
   })
-})
+});
 
-// setupRedisSubscriptions()
-// setupSocketIOListeners()
+/*
+(async () => {
+  try {
+    await Promise.all([
+      setupRedisSubscriptions()
+      setupSocketIOListeners()
+      subscribeToRedisExpiredKeyEvents()
+      configureVault()
+    ])
+    server.listen()
+    io.listen()
+  } catch (err) {
+    debug.api(err)
+    process.exit(1)
+  }
+})()
+*/
 
-// subscribeToRedisExpiredKeyEvents()
-server.listen(environment.api.port, () => debug.api(`server listening on port ${environment.api.port}`))
-io.listen(server)
+(async () => {
+  try {
+    if (!process.env.VAULT_APPROLE_ROLE_ID) {
+      throw new Error('VAULT_APPROLE_ROLE_ID environment variable must be provided.')
+    }
+    if (!process.env.VAULT_APPROLE_ROLE_SECRET_WRAP_TOKEN) {
+      throw new Error('VAULT_APPROLE_ROLE_SECRET_WRAP_TOKEN environment variable must be provided.')
+    }
+    const { apiVersion, protocol, host, port } = environment.vault
+    const endpoint = `${protocol}${host}:${port}`
+    const token = process.env.VAULT_APPROLE_ROLE_SECRET_WRAP_TOKEN
+    const tempVault = nodeVault({ apiVersion, endpoint, token })
+    const initializedResponse = await tempVault.initialized()
+    if (!initializedResponse.initialized) {
+      throw new Error('Vault is not initialized.')
+    }
+    const statusResponse = await tempVault.status()
+    if (statusResponse.sealed) {
+      throw new Error('Vault is sealed.')
+    }
+    const unwrapResponse = await tempVault.unwrap()
+    const roleId = process.env.VAULT_APPROLE_ROLE_ID
+    const secretId = unwrapResponse.data.secret_id
+    const approleLoginResponse = await vault.approleLogin({
+      role_id: roleId,
+      secret_id: secretId
+    })
+    vault.token = approleLoginResponse.auth.client_token // eslint-disable-line require-atomic-updates
+    server.listen(environment.api.port, () => debug.api(`server listening on port ${environment.api.port}`))
+    io.listen(server)
+    debug.socketio('socket server listening')
+  } catch (err) {
+    debug.api(err)
+    process.exit(1)
+  }
+})()
+
+// server.listen(environment.api.port, () => debug.api(`server listening on port ${environment.api.port}`))
+// io.listen(server)
